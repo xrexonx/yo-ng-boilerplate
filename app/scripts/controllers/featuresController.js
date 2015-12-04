4171c3fdc5ce3aed5ebb148538457a5d0cceb2bc
rexon <rexondelosreyes88@gmail.com>
2015-12-04 13:47:16 +0800

    Refactor templates according to standards

--- app/scripts/controllers/featuresController.js
+++ app/scripts/controllers/featuresController.js
@@ -5,7 +5,6 @@
         .module('AppControllers')
         .controller('featuresController', function ($scope, $stateParams) {
 
-
             console.log($stateParams);
             console.log($scope);
 
