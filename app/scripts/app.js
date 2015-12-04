9edebb34c6eebe3fa5656b79bbd1bfaa02342002
rexon <rexondelosreyes88@gmail.com>
2015-12-04 11:15:20 +0800

    Configuring routes and dynamic templates in index #happy

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,24 +8,55 @@
  *
  * Main module of the application.
  */
+
+//var myApp = angular.module('myApp', ['ui.router']);
 angular
-  .module('yoNgBolierplateApp', [
-    'ngAnimate',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+  .module('yoNgBoilerPlateApp', ['ui.router'])
+
+
+    .config(function($stateProvider, $urlRouterProvider) {
+
+      console.log($stateProvider);
+      console.log($urlRouterProvider);
+
+      function _getView (viewName) {
+        return 'views/'+viewName+'.html';
+      }
+
+
+
+      $stateProvider
+          .state('/', {
+            url: '/',
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+          })
+          .state('project', {
+            url: '/:id/:view',
+            templateUrl: _getView('project'),
+            controllerProvider: function($stateParams) {
+              return $stateParams.view + 'Controller';
+            }
+          });
+
+      $urlRouterProvider.otherwise('/');
+
+
+  //.config(function ($routeProvider) {
+  //    console.log($routeProvider);
+  //  $routeProvider
+  //    .when('/', {
+  //      templateUrl: 'views/main.html',
+  //      controller: 'MainCtrl',
+  //      controllerAs: 'main'
+  //    })
+  //    .when('/about', {
+  //      templateUrl: 'views/about.html',
+  //      controller: 'AboutCtrl',
+  //      controllerAs: 'about'
+  //    })
+  //    .otherwise({
+  //      redirectTo: '/'
+  //    });
   });
--- gulpfile.js
+++ gulpfile.js
@@ -21,10 +21,6 @@ var paths = {
   testRequire: [
     yeoman.app + '/bower_components/angular/angular.js',
     yeoman.app + '/bower_components/angular-mocks/angular-mocks.js',
-    yeoman.app + '/bower_components/angular-resource/angular-resource.js',
-    yeoman.app + '/bower_components/angular-cookies/angular-cookies.js',
-    yeoman.app + '/bower_components/angular-sanitize/angular-sanitize.js',
-    yeoman.app + '/bower_components/angular-route/angular-route.js',
     'test/mock/**/*.js',
     'test/spec/**/*.js'
   ],
